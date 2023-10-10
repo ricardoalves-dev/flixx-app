d676354ca6cf55170c5aa2c16a83220db15788a4
TEKSYSTEM\Ricardo Alves <ricardoalves@teksystem.com.br>
2023-10-10 22:38:51 +0100

    Get Popular Movies

--- js/script.js
+++ js/script.js
@@ -2,6 +2,15 @@ const app = {
   currentPage: window.location.pathname,
 };
 
+const api = {
+  url: 'https://api.themoviedb.org/3',
+  token:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZjFjZTQyN2Y0MjZkNGY2ZDdlZDY1M2M1ODc5NDI2NCIsInN1YiI6IjY1MjBhNGRjYzUwYWQyMDEyYzFjOTVlOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.B_rwN6U7ofnmC2y2VeZqJzeo_LJN60H-1xNElbd8Czk',
+  paths: {
+    popularMovies: '/movie/popular',
+  },
+};
+
 function highlightCurrentMenu() {
   document.querySelectorAll('.nav-link').forEach((menu) => {
     if (menu.getAttribute('href') === app.currentPage) {
@@ -16,6 +25,7 @@ function init() {
     case '/':
     case 'index.html':
       console.log('Home');
+      getPopularMovies();
       break;
 
     case 'movie-details.html':
@@ -36,6 +46,94 @@ function init() {
   }
 
   highlightCurrentMenu();
+}
+
+// GET CABEÇALHO
+function getHeader() {
+  return {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${api.token}`,
+  };
+}
+
+// GET GENERICO
+async function get(endpoint) {
+  const resp = await fetch(api.url + endpoint, {
+    method: 'GET',
+    headers: getHeader(),
+  });
+
+  console.log(resp.json);
+  return resp.json();
+}
+
+// GET POPULAR MOVIES
+async function getPopularMovies() {
+  const data = await get(api.paths.popularMovies);
+  const movies = data.results;
+  console.log(movies);
+  const popularMoviesContainer = document.querySelector('#popular-movies');
+  popularMoviesContainer.innerHTML = '';
+
+  movies.forEach((movie) => setPopularMovie(movie, popularMoviesContainer));
+}
+
+function setPopularMovie(movie, popularMoviesContainer) {
+  const card = createElement('div', ['card']);
+  const link = createElement(
+    'a',
+    [],
+    [
+      {
+        name: 'href',
+        value: `./movie-details.html?id=${movie.id}`,
+      },
+    ]
+  );
+  const image = createElement(
+    'img',
+    ['card-img-top'],
+    [
+      {
+        name: 'src',
+        value: './images/no-image.jpg',
+      },
+      {
+        name: 'alt',
+        value: 'Movie Title',
+      },
+    ]
+  );
+
+  const cardBody = createElement('div', ['card-body']);
+  const cardTitle = createElement('h5', ['card-title']);
+  cardTitle.innerText = movie.title;
+
+  const cardText = createElement('p', ['card-text']);
+  const textMuted = createElement('small', ['text-muted']);
+  textMuted.innerText = `Release: ${movie.release_date}`;
+
+  card.appendChild(link);
+  link.appendChild(image);
+  card.appendChild(cardBody);
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(cardText);
+  cardText.appendChild(textMuted);
+  popularMoviesContainer.appendChild(card);
+}
+
+function createElement(tag, classes, attributes) {
+  const element = document.createElement(tag);
+
+  if (classes !== undefined) {
+    classes.forEach((c) => element.classList.add(c));
+  }
+
+  if (attributes !== undefined) {
+    attributes.forEach((a) => element.setAttribute(a.name, a.value));
+  }
+
+  return element;
 }
 
 document.addEventListener('DOMContentLoaded', init);
